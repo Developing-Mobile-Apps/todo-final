97b398f330cbcc7e43b8ea39be0cb3dd7b7c2567
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-30 09:02:32 +0545

    feat: create type converter
    Create a Converter class to convert the Priority and Date property of Todo model. This prevents the TodoRoomDatabase to save those property in a type that is compaitable with the sqlite dabase used by the RoomDatabase

---
+++ app/src/main/java/com/example/todofinal/util/Converter.java
@@ -0,0 +1,31 @@
+package com.example.todofinal.util;
+
+import androidx.room.TypeConverter;
+
+import com.example.todofinal.model.Priority;
+
+import java.util.Date;
+
+public class Converter {
+    @TypeConverter
+    public static Date fromTimestampToDate(Long timestamp) {
+        return (timestamp == null) ? null : new Date(timestamp);
+    }
+
+    @TypeConverter
+    public static Long fromDateToTimestamp(Date date) {
+        return (date == null) ? null : date.getTime();
+    }
+
+    @TypeConverter
+    public static String fromPriority(Priority priority) {
+        return (priority == null) ? null : priority.name();
+    }
+
+    @TypeConverter
+    public static Priority toPriority(String priority) {
+        return (priority == null) ? null : Priority.valueOf(priority);
+    }
+
+
+}
--- app/src/main/java/com/example/todofinal/util/TodoRoomDatabase.java
+++ app/src/main/java/com/example/todofinal/util/TodoRoomDatabase.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.todofinal.data.TodoDao;
@@ -15,6 +16,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Todo.class}, version = 1, exportSchema = false)
+@TypeConverters({Converter.class})
 public abstract class TodoRoomDatabase extends RoomDatabase {
     public static final int NUMBER_OF_THREADS = 4;
     public static final String DATABASE_NAME = "todo_db";
