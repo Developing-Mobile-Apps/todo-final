d5469556536b4f640031180cd8188e589309dfa5
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-30 20:06:31 +0545

    fix: fix the calendar date calculation
    Reset calendar to current date on every onClick event so that dates for new today, tomorrow and nextweek is calculated from the current date not the last selected date

--- app/src/main/java/com/example/todofinal/BottomSheetFragment.java
+++ app/src/main/java/com/example/todofinal/BottomSheetFragment.java
@@ -173,7 +173,8 @@ public class BottomSheetFragment extends BottomSheetDialogFragment implements Vi
     @Override
     public void onClick(View view) {
         int id = view.getId();
-
+        // reset the calendar to current date
+        calendar = Calendar.getInstance();
         if (id == R.id.today_chip) {
             calendar.add(Calendar.DAY_OF_YEAR, 0);
         } else if (id == R.id.tomorrow_chip) {
