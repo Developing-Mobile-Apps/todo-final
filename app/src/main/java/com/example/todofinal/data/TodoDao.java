ba5fd4c8fe90d425f0d2299a365c155e0b6f3260
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-30 08:44:47 +0545

    feat: create dao and database
    Create a TodoDao and TodoRoomDatabase for the app

---
+++ app/src/main/java/com/example/todofinal/data/TodoDao.java
@@ -0,0 +1,34 @@
+package com.example.todofinal.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.todofinal.model.Todo;
+
+import java.util.List;
+
+@Dao
+public interface TodoDao {
+
+    @Insert
+    void insertTodo(Todo todo);
+
+    @Update
+    void update(Todo todo);
+
+    @Delete
+    void delete(Todo todo);
+
+    @Query("SELECT * FROM todo_table WHERE todo_id == :id")
+    LiveData<Todo> get(long id);
+
+    @Query("SELECT * FROM todo_table")
+    LiveData<List<Todo>> getAll();
+
+    @Query("DELETE FROM todo_table")
+    void deleteAll();
+}
---
+++ app/src/main/java/com/example/todofinal/util/TodoRoomDatabase.java
@@ -0,0 +1,54 @@
+package com.example.todofinal.util;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.todofinal.data.TodoDao;
+import com.example.todofinal.model.Todo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Todo.class}, version = 1, exportSchema = false)
+public abstract class TodoRoomDatabase extends RoomDatabase {
+    public static final int NUMBER_OF_THREADS = 4;
+    public static final String DATABASE_NAME = "todo_db";
+    public static final ExecutorService databaseWriteExecutor
+            = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    public static volatile TodoRoomDatabase instance;
+    public static final RoomDatabase.Callback roomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                    super.onCreate(db);
+                    databaseWriteExecutor.execute(() -> {
+                        // invoke DAO, and write
+                        TodoDao todoDao = instance.todoDao();
+                        todoDao.deleteAll(); // clean table
+                    });
+                }
+            };
+
+    public static TodoRoomDatabase getDatabase(final Context context) {
+        if (instance == null) {
+            synchronized (TodoRoomDatabase.class) {
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            TodoRoomDatabase.class, DATABASE_NAME)
+                            .addCallback(roomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public abstract TodoDao todoDao();
+
+}
